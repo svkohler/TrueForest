b55415aa58799576757d979921b457a42ab043c5
svkohler <svenkohler1995@hotmail.com>
2022-03-14 13:57:42 +0100

    .

--- GEE/website_test.py
+++ GEE/website_test.py
@@ -1,48 +1,84 @@
+from rasterio import windows
+import rasterio as rio
+from itertools import product
+import os
+from pyproj import Proj, transform
+from affine import Affine
+import rasterio
+from rasterio.windows import Window
 import ee
 import geemap
 import sys
 import numpy as np
+from PIL import Image
+from skimage import io
+import pickle
 
-# ee.Authenticate()
-ee.Initialize()
 
-# 30.93605976603369  # 35.236  #  32.60215788172933 # 30.025215750666348
-START_NORTH = 36.32
-# 30.98934617733734  # 35.377  # 32.738568513477766  # 30.139294239374312
-END_SOUTH = 37.05
-# -89.2068111032426  # -86.06  #  -112.00920345515958 # -84.54255356391563
-START_WEST = -120.38
-# -89.14484134616252  # -85.89  # -111.84681179256192  # -84.41724075873985
-END_EAST = -119.62
-NR_STEPS = 2
+# fname = '/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/raw/NAIP/Central_Valley/train/NAIP_image_36.45_-120.05.tif'
 
-step_size = abs((START_NORTH-END_SOUTH)/NR_STEPS)
 
-north_to_south = np.linspace(START_NORTH, END_SOUTH, NR_STEPS+1)
-west_to_east = np.linspace(START_WEST, END_EAST, NR_STEPS+1)
+# in_path = '/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/raw/NAIP/Central_Valley/train/'
+# input_filename = 'NAIP_image_36.45_-120.05.tif'
 
-for ns in north_to_south[:len(north_to_south)-1]:
-    for we in west_to_east[:len(west_to_east)-1]:
-        area = ee.Geometry.Rectangle(
-            we,
-            ns,
-            we+step_size,
-            ns+step_size)
-        naip = ee.ImageCollection(
-            'USDA/NAIP/DOQQ').filterBounds(area).filterDate('2017-01-01', '2018-12-31')
+# out_path = '/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/'
+# output_filename = 'tile_{}-{}.tif'
 
-        naip_mosaic = naip.mosaic().visualize(
-            bands=['R', 'G', 'B'])
 
-        task = ee.batch.Export.image.toDrive(
-            image=naip_mosaic,
-            description='NAIP_image_' + str(ns) + '_'+str(we),
-            scale=1,
-            folder='NAIP',
-            region=area,
-            maxPixels=10e12,
-            fileFormat='GeoTIFF'
-        )
+# def get_tiles(ds, width=448, height=448):
+#     nols, nrows = ds.meta['width'], ds.meta['height']
+#     offsets = product(range(0, nols, width), range(0, nrows, height))
+#     big_window = windows.Window(col_off=0, row_off=0, width=nols, height=nrows)
+#     for col_off, row_off in offsets:
+#         window = windows.Window(col_off=col_off, row_off=row_off,
+#                                 width=width, height=height).intersection(big_window)
+#         transform = windows.transform(window, ds.transform)
+#         yield window, transform
 
-        task.start()
-        print('task done.')
+
+# with rio.open(os.path.join(in_path, input_filename)) as inds:
+#     tile_width, tile_height = 448, 448
+
+#     meta = inds.meta.copy()
+
+#     for window, transform in get_tiles(inds):
+#         print(window)
+#         meta['transform'] = transform
+#         meta['width'], meta['height'] = window.width, window.height
+#         outpath = os.path.join(out_path, output_filename.format(
+#             int(window.col_off), int(window.row_off)))
+#         with rio.open(outpath, 'w', **meta) as outds:
+#             outds.write(inds.read(window=window))
+
+
+# imgs = os.listdir(
+#     '/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/')
+
+
+# coordinates_images = {}
+# counter = 0
+# for img in imgs:
+#     dataset = rasterio.open(
+#         '/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/'+img)
+#     coordinates = {}
+#     coordinates['left'] = dataset.bounds[0]
+#     coordinates['bottom'] = dataset.bounds[1]
+#     coordinates['right'] = dataset.bounds[2]
+#     coordinates['top'] = dataset.bounds[3]
+#     coordinates_images[img] = coordinates
+#     pil = Image.fromarray(
+#         np.uint8(io.imread('/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/'+img))[:, :, :3])
+#     pil.save(
+#         '/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/'+img[:-3]+'png')
+#     os.remove(
+#         '/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/'+img)
+#     print(f'done {counter}/{len(imgs)}')
+#     counter += 1
+
+# with open('/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/coordinates_images.pickle', 'wb') as handle:
+#     pickle.dump(coordinates_images, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open('/home/svkohler/OneDrive/Desktop/Masterthesis/Code/TrueForest/data/website_test/coordinates_images.pickle', 'rb') as handle:
+    b = pickle.load(handle)
+
+print(b['tile_448-3584.tif'])
